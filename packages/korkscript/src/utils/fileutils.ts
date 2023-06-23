7b8218cc4835287ce4836da6b5d78809c7fa55ba
Zffu <radi0o0@outlook.com>
2023-06-24 01:07:23 +0200

    Update fileutils.ts - test

--- packages/korkscript/src/utils/fileutils.ts
+++ packages/korkscript/src/utils/fileutils.ts
@@ -1,6 +1,9 @@
+const path = require("path")
+const fs = require("fs")
+
 const getFileList = async (dirname: string) => {
     let files: string[]= [];
-    const items = await readdir(dirName, { withFileTypes: true });
+    const items = await fs.readdir(dirName, { withFileTypes: true });
 
     for (const item: string of items) {
         if (item.isDirectory()) {
