341cc79497b8e8c74396dba9965647e9302c5f0b
Zffu <radi0o0@outlook.com>
2023-06-24 12:58:12 +0200

    Update index.ts

--- packages/korkscript/src/expressions/index.ts
+++ packages/korkscript/src/expressions/index.ts
@@ -5,5 +5,13 @@ export const defaultModules = [require("./func")]
 
 
 export function getDefaultExpressions(): any[] {
-    
+    let ex: any[] = []
+     defaultModules.forEach(module => {
+      Object.keys(key => {
+        if(key["expressionType"] != undefined) {
+          ex.push(key)
+        }
+      })
+    })
+    return ex
 }
