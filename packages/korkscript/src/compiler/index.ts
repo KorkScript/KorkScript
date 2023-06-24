d6813798f7034c594321d8ddb84b28f5f27e6e3a
Zffu <radi0o0@outlook.com>
2023-06-24 14:18:22 +0200

    Update index.ts - test

--- packages/korkscript/src/compiler/index.ts
+++ packages/korkscript/src/compiler/index.ts
@@ -10,12 +10,10 @@ export async function runCompiler(p: string): Promise<CompilerResponse> {
     const promise: Promise<CompilerResponse> = new Promise((resolve, reject) => {
         const compiler: Compiler = new Compiler(p)
         compiler.start()
-        let files: string[] = [];
         getFilesToCompile(p).then((f: string[]) => {
-            files = f;
             logger.info("Found " + files.length + " files to compile!")
-            files.forEach((file: string) => async function() {
-              compileFile(file)
+            f.forEach((file: string) => async function() {
+              await compileFile(file)
             })
             resolve(compiler.stop(true))
         })
@@ -40,24 +38,25 @@ export function getFilesToCompile(p: string): Promise<string[]> {
     return promise;
 }
 
+ 
 
-export async function compileFile(dir: string): Promise<any[]> {
+export  function compileFile(dir: string): Promise<any[]> {
    const promise: Promise<any[]> = new Promise((resolve, reject) => async function () {
    console.log("hello")
    try {
-        const content = await fs.readFile(dir, { encoding: 'utf8' });
+        fs.readFile(dir, { encoding: 'utf8' }).then((content: string) => {
+          let useTypescript: boolean = false;
 
-        let useTypescript: boolean = false;
-
-        if(content.includes("#include typescript")) {
-          useTypescript = true;
-        }
+          if(content.includes("#include typescript")) {
+            useTypescript = true;
+          }
         
-        let expressions: any[] = expression.getDefaultExpressions()
+          let expressions: any[] = expression.getDefaultExpressions()
 
-        console.log(expressions.toString())
+          console.log(expressions.toString())
 
-        resolve(expressions)
+          resolve(expressions)
+        })
 
     } catch (err) {
         console.log(err);
