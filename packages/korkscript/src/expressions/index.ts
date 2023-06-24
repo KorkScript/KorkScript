997165b7fabf36c49299a9b9ac3cf1a2deccea5b
Zffu <radi0o0@outlook.com>
2023-06-24 12:56:12 +0200

    Update index.ts

--- packages/korkscript/src/expressions/index.ts
+++ packages/korkscript/src/expressions/index.ts
@@ -1,17 +1,9 @@
 const logger = require("../logger")
 import {Expression, BasicExpression} from "./types"
 
-export const defaultModules = [require("./time")]
+export const defaultModules = [require("./func")]
 
-export function parseExpression(expression: string) {
-    defaultModules.forEach(module => {
-        Object.keys(module).forEach((key:any) => {
-            if(key["expressionType"] != undefined) {
-                
-            }
-            else {
-                logger.warn("Expression " + key + "is not valid!")
-            }
-        }) 
-    })
+
+export function getDefaultExpressions(): any[] {
+    
 }
