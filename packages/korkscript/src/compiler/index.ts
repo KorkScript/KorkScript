0c8673e3684de565e08deaacada380b3d6af043e
Zffu <radi0o0@outlook.com>
2023-06-24 14:09:25 +0200

    Update index.ts - test

--- packages/korkscript/src/compiler/index.ts
+++ packages/korkscript/src/compiler/index.ts
@@ -42,7 +42,7 @@ export function getFilesToCompile(p: string): Promise<string[]> {
 
 
 export async function compileFile(dir: string): Promise<any[]> {
-   const promise: Promise<any[]> = new Promise((resolve, reject)) => {
+   const promise: Promise<any[]> = new Promise((resolve, reject) => {
    try {
         const content = await fs.readFile(dir, { encoding: 'utf8' });
 
